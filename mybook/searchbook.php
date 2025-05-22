31dfdd90f71418b74b9a04c008175d81185c00f5
Robert Carlos Sinaga <168253924+RecursiveFunc@users.noreply.github.com>
2025-05-22 15:20:00 +0700

    Second Commit
    Updated searching based on Writer, Title and Genre

--- mybook/searchbook.php
+++ mybook/searchbook.php
@@ -6,28 +6,30 @@ if (isset($_GET['query'])) {
     $query = "%" . $conn->real_escape_string($_GET['query']) . "%";
 
     $sql = "
-        SELECT 
-            b.id, 
-            b.judul AS title, 
-            b.penulis AS author, 
-            GROUP_CONCAT(g.nama_genre SEPARATOR ', ') AS genres
-        FROM 
-            books AS b
-        LEFT JOIN 
-            books_genres AS bg ON b.id = bg.book_id
-        LEFT JOIN 
-            genres AS g ON bg.genre_id = g.id
-        WHERE 
-            b.judul LIKE ? OR b.penulis LIKE ?
-        GROUP BY 
-            b.id
+    SELECT 
+        b.id, 
+        b.judul AS title, 
+        b.penulis AS author, 
+        GROUP_CONCAT(g.nama_genre SEPARATOR ', ') AS genres
+    FROM 
+        books AS b
+    LEFT JOIN 
+        books_genres AS bg ON b.id = bg.book_id
+    LEFT JOIN 
+        genres AS g ON bg.genre_id = g.id
+    WHERE 
+        b.judul LIKE ? OR 
+        b.penulis LIKE ? OR
+        g.nama_genre LIKE ? 
+    GROUP BY 
+        b.id
     ";
 
     $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $query, $query);
+    $stmt->bind_param("sss", $query, $query, $query);
     $stmt->execute();
     $result = $stmt->get_result();
-    
+
     $books = [];
     while ($row = $result->fetch_assoc()) {
         $books[] = $row;
@@ -37,4 +39,3 @@ if (isset($_GET['query'])) {
     echo json_encode($books);
     exit;
 }
-?>
